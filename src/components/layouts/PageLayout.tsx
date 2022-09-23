6537713c000174969938ae02d37866208ff9ff33
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-09-23 12:19:37 +0300

    add header and main to pageLayout

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "../header/Header";
 
 const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
 };
 
 export default PageLayout;
