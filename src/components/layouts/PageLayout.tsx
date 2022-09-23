c839fe2bc72c98a3f9e2b5826b62ed83d3d253b6
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-09-23 18:54:54 +0300

    add container for main in pageLayout

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../header/Header";
+import Container from "@mui/material/Container";
 
 const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div>
       <Header />
-      <main>{children}</main>
+
+      <main>
+        <Container maxWidth="lg">{children}</Container>
+      </main>
     </div>
   );
 };
