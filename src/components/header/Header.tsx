9152cac8b8f2e6a2530dfc8d562a714ccc18cda3
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-09-23 12:37:27 +0300

    create reset button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DropDown from "./DropDown";
+import ResetButton from "./ResetButton";
 
 const Header = () => {
   return (
     <header>
       <DropDown />
+      <ResetButton />
     </header>
   );
 };
---
+++ src/components/header/ResetButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const ResetButton = () => {
+  const resetFilters = () => {
+    console.log("reset");
+  };
+  return (
+    <Button variant="contained" onClick={resetFilters}>
+      Reset
+    </Button>
+  );
+};
+
+export default ResetButton;
