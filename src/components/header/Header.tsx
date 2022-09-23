d0560d9efcc40574c836f8d4faf2f423ed1d447a
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-09-23 12:47:10 +0300

    create links for navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DropDown from "./DropDown";
 import ResetButton from "./ResetButton";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <header>
       <DropDown />
       <ResetButton />
+      <Navigation />
     </header>
   );
 };
