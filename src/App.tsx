985655e15423c6bc7f906c50594b2286c59e7d6c
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-09-23 12:08:46 +0300

    add navigation between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import PieChartPage from "./pages/PieChartPage";
+import LineChartPage from "./pages/LineChartPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="pie-chart" />} />
+      <Route path="pie-chart" element={<PieChartPage />} />
+      <Route path="line-chart" element={<LineChartPage />} />
+      <Route path="*" element={<Navigate to="pie-chart" />} />
+    </Routes>
+  );
 }
 
 export default App;
