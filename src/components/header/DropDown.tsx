736cd53eb70dd679ca02139297a9e1b405b058c8
Molitvin Yevhenii <molitvin.ye@gmail.com>
2022-09-23 17:34:03 +0300

    add reducers: setParameter, setDataRange, setCategories and resetFilter

--- src/components/header/DropDown.tsx
+++ src/components/header/DropDown.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CHART_PARAMETERS } from "../../store/constants";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import styles from "./DropDown.module.css";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { dataSlice } from "../../store/reducers/DataSlice";
 
 const DropDown = () => {
-  const [parameter, setParameter] = React.useState(CHART_PARAMETERS[0]);
+  const dispatch = useAppDispatch();
+  const { parameter } = useAppSelector((state) => state.dataReducer);
 
   const handleChange = (event: SelectChangeEvent) => {
-    setParameter(event.target.value);
+    dispatch(dataSlice.actions.setParameter(event.target.value));
   };
 
   return (
--- src/store/reducers/DataSlice.ts
+++ src/store/reducers/DataSlice.ts
@@ -4,13 +4,31 @@ import { IData } from "../../modules/IData"
 interface DataState {
   data: IData[],
   isLoading: boolean,
-  error: string
+  error: string,
+  parameter: string,
+  dateRange: {
+    all: string[],
+    filter: string[]
+  },
+  categories: {
+    all: string[],
+    filter: string[]
+  }
 }
 
 export const initialState: DataState  = {
   data: [],
   isLoading: false,
-  error: ""
+  error: "",
+  parameter: "markdown",
+  dateRange: {
+    all: [],
+    filter: []
+  },
+  categories: {
+    all: [],
+    filter: []
+  }
 }
 
 export const dataSlice = createSlice({
@@ -20,14 +38,65 @@ export const dataSlice = createSlice({
     dataFetching(state) {
       state.isLoading = true
     },
+
     dataFetchingSuccess(state, action: PayloadAction<IData[]>) {
       state.isLoading = false
       state.error = ''
       state.data = action.payload
+
+      const dateRange: string[] = []
+      const categories: string[] = []
+
+      action.payload.forEach((row) => {
+        const week = row.week_ref
+        const category = row.category_desc
+        if (week && !dateRange.includes(week)) dateRange.push(week)
+        if (category && !categories.includes(category)) categories.push(category)
+      })
+
+      state.dateRange = {
+        all: dateRange,
+        filter: dateRange
+      }
+
+      state.categories = {
+        all: categories,
+        filter: categories
+      }
     },
+
     dataFetchingError(state, action: PayloadAction<string>) {
       state.isLoading = false
       state.error = action.payload
+    },
+
+    setParameter(state, action: PayloadAction<string>) {
+      state.parameter = action.payload
+    },
+
+    setDataRange(state, action: PayloadAction<string[]>) {
+      state.dateRange = {
+        ...state.dateRange,
+        filter: action.payload
+      }
+    },
+
+    setCategories(state, action: PayloadAction<string[]>) {
+      state.categories = {
+        ...state.categories,
+        filter: action.payload
+      }
+    },
+
+    resetFilters(state) {
+      state.dateRange = {
+        all: state.categories.all,
+        filter: state.categories.all
+      }
+      state.categories = {
+        all: state.categories.all,
+        filter: state.categories.all
+      }
     }
   }
 })
